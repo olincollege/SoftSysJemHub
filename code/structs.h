24ab851bfab56a276e0befc9c32cf09d0c885690
Miles Mezaki <mmezaki@olin.edu>
2022-04-15 11:14:48 -0400

    Define graph, tree, and commit_t structs

--- code/structs.h
+++ code/structs.h
@@ -1,8 +1,21 @@
-#include <glib-2.0>
+typedef struct graphnode {
+    struct graphnode * descendants;
+} GraphNode;
 
 typedef struct {
-    parents: GArray // This should be the state of HEAD at the type of committing
-    author: char * // Person who commits
-    message: char * // Commit MSG
-    snapshot: GHashTable // Snapshot containing actual information to update the current HEAD + make changes
-} branch // commit object
+    FILE ** snap; // this type might change, right now I was thinking an array of file pointers
+    struct TreeNode * descendants;
+} TreeNode;
+
+typedef struct {
+    TreeNode tree_head;
+} SnapTree;
+
+typedef struct {
+    GraphNode * parents; // This should be the state of HEAD at the type of committing
+    char * author; // Person who commits
+    char * message; // Commit MSG
+    SnapTree snapshots; // Snapshot containing actual information to update the current HEAD + make changes
+} commit_t; // commit object
+
+
