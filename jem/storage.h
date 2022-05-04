3dcf3c807db12712c210a7149ecfe559758311c7
Miles Mezaki <mmezaki@olin.edu>
2022-05-04 16:14:42 -0400

    Add copy files to jem to header

--- jem/storage.h
+++ jem/storage.h
@@ -10,6 +10,8 @@ void serialize_size(unsigned char** buffer, size_t size);
 
 void deserialize_size(unsigned char** buffer, size_t *size);
 
+void copy_file_to_jem(char * src_filepath, reference_t *file_ref);
+
 // save the buffer to disk
 reference_t *write_buffer_to_disk(unsigned char** buffer, size_t size);
 
